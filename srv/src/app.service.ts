d8c2de537849d943f355d3dcf5286e1a390738ec
fcamblor <fcamblor@gmail.com>
2022-10-04 00:42:42 +0200

    added dependency from srv module to shared module

--- srv/src/app.service.ts
+++ srv/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import {Hello} from '@shared/domains'
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello '+new Hello().who;
   }
 }
